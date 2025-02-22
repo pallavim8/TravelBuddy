174c16266150fcdef90c3f9b45782db236f8e726
pallavim8 <103335683+pallavim8@users.noreply.github.com>
2025-02-22 10:04:26 -0500

    Initial Commit

---
+++ MealBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MealBuddy
+//
+//  Created by Pallavi Mamillapalli on 2/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MealBuddy/MealBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealBuddyApp.swift
+//  MealBuddy
+//
+//  Created by Pallavi Mamillapalli on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MealBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
