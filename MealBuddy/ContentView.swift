390461a271b11fc4c91fdf85d68fb2c3ee0e201f
arathiponneth <qbj2sw@virginia.edu>
2025-02-22 16:44:30 -0500

    added splash screen

--- MealBuddy/ContentView.swift
+++ MealBuddy/ContentView.swift
@@ -3,80 +3,166 @@ import SwiftUI
 
 
 extension Color {
+
     init(hex: String) {
+
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+
         var int: UInt64 = 0
+
         Scanner(string: hex).scanHexInt64(&int)
+
         let r, g, b: Double
+
         if hex.count == 6 {
+
             r = Double((int >> 16) & 0xFF) / 255.0
+
             g = Double((int >> 8) & 0xFF) / 255.0
+
             b = Double(int & 0xFF) / 255.0
+
         } else {
+
             r = 1.0
+
             g = 1.0
+
             b = 1.0
+
         }
+
         self.init(red: r, green: g, blue: b)
+
     }
+
 }
 
+
+
 struct ContentView: View {
+
+    @State private var scale = 0.0
+
+    @State private var opacityText = 0.0
+
+    @State private var opacityButton = 0.0
+
+    @State private var offsetText = 20.0
+
+    @State private var offsetButton = 20.0
+
+
+
     var body: some View {
-        NavigationStack{
+
+        NavigationStack {
+
             VStack(spacing: 20) {
+
                 Image("mealbuddy")
+
                     .resizable()
+
                     .scaledToFit()
+
                     .frame(width: 300, height: 300)
+
                 
+
                 Text("MEALBUDDY")
+
                     .font(.system(size: 40, weight: .heavy))
+
                     .foregroundColor(.black)
+
                     .tracking(1)
+
+                    .offset(y: -5)
+
                 
-                
-                
-                Text("Connect with foodies nearby\nfor culinary adventures")
-                    .font(.system(size: 18, weight: .medium))
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color(hex: "#554F48"))
-                
-                
-                
-                //            Button(action: {
-                //                print("Join Now tapped")
-                //            }) {
-                //
-                //                Text("JOIN NOW")
-                //                    .font(.system(size: 15, weight: .medium))
-                //                    .frame(maxWidth: .infinity)
-                //                    .padding()
-                //                    .background(Color(hex: "#66574A"))
-                //                    .foregroundColor(Color(hex: "#F6F3EC"))
-                //                    .cornerRadius(30)
-                //            }
-                
-                
-                NavigationLink(destination: AuthView()) {
-                    Text("JOIN NOW")
-                        .font(.system(size: 15, weight: .medium))
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color(hex: "#66574A"))
-                        .foregroundColor(Color(hex: "#F6F3EC"))
-                        .cornerRadius(30)
+
+                VStack {
+
+                    Text("Connect with foodies nearby\nfor culinary adventures")
+
+                        .font(.system(size: 18, weight: .medium))
+
+                        .multilineTextAlignment(.center)
+
+                        .foregroundColor(Color(hex: "#554F48"))
+
+                        .opacity(opacityText)
+
+                        .offset(y: offsetText)
+
+                    
+
+                    NavigationLink(destination: AuthView()) {
+
+                        Text("JOIN NOW")
+
+                            .font(.system(size: 15, weight: .medium))
+
+                            .frame(maxWidth: .infinity)
+
+                            .padding()
+
+                            .background(Color(hex: "#66574A"))
+
+                            .foregroundColor(Color(hex: "#F6F3EC"))
+
+                            .cornerRadius(30)
+
+                            .opacity(opacityButton)
+
+                            .offset(y: offsetButton)
+
+                    }
+
+                    .padding(.horizontal, 40)
+
+                    .padding(.vertical, 30)
+
                 }
-                
-                .padding(.horizontal, 40)
-                .padding(.vertical, 30)
+
+                .scaleEffect(scale)
+
+                .onAppear {
+
+                    withAnimation(.easeIn(duration: 0.7)) {
+
+                        self.scale = 1
+
+                    }
+
+                    withAnimation(.easeIn(duration: 0.7).delay(0.3)) {
+
+                        self.opacityText = 1
+
+                        self.offsetText = 0
+
+                    }
+
+                    withAnimation(.easeIn(duration: 0.7).delay(0.6)) {
+
+                        self.opacityButton = 1
+
+                        self.offsetButton = 0
+
+                    }
+
+                }
+
             }
-            
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(hex: "#EEE2D2"))
-        .navigationBarBackButtonHidden(true)
+
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+            .background(Color(hex: "#EEE2D2"))
+
+            .navigationBarBackButtonHidden(true)
+
         }
-            
 
     }
 
@@ -85,5 +171,7 @@ struct ContentView: View {
 
 
 #Preview {
+
     ContentView()
+
 }
---
+++ MealBuddy/SplashScreen.swift
@@ -0,0 +1,63 @@
+//
+//  SplashScreen.swift
+//  MealBuddy
+//
+//  Created by Chinju on 2/22/25.
+//
+
+
+import SwiftUI
+
+
+struct SplashScreen: View {
+    @State private var scale = 0.7
+    @State private var offsetY: CGFloat = 0 // Start at the center
+    @Binding var isActive: Bool
+
+    
+
+    var body: some View {
+        VStack {
+            VStack {
+                Image("mealbuddy")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+
+
+                Text("MEALBUDDY")
+                    .font(.system(size: 40, weight: .heavy))
+                    .foregroundColor(.black)
+                    .tracking(1)
+
+            }
+
+            .scaleEffect(scale)
+            .offset(y: offsetY) // Apply the offset
+            .onAppear {
+
+                withAnimation(.easeIn(duration: 0.7)) {
+                    self.scale = 1
+                    self.offsetY = -95 // Move the content up by 50 points during the animation
+
+                }
+
+            }
+
+        }
+
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+
+        }
+
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(hex: "#EEE2D2"))
+
+    }
+
+}
